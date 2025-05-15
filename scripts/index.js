2aa900ffd7c540e3af3f6b3151fab3b3ad9d0ceb
James Junior Baah <jjbaah068@gmail.com>
2025-05-15 10:40:18 -0700

    portfolio update

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+// detect scroll event on document 
+document.addEventListener('scroll', function() {
+    // console.log(window.scrollY);
+    if (window.scrollY > 70) {
+        // change navbar bg to blue
+        document.getElementById('navbar').style.backgroundColor = '#0779E4';
+    } else {
+        // change navbar bg to transparent
+        document.getElementById('navbar').style.backgroundColor = 'transparent';
+    }
+});
